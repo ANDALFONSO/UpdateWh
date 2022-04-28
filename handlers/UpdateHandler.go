2e98cf0d6c267b09013bf494a7d4fe8e7d0db0e4
ANDALFONSO <andresfelipe.alfonso@mercadolibre.com.co>
2022-04-28 11:06:21 -0500

    se agrega parametro para processar o no

--- handlers/UpdateHandler.go
+++ handlers/UpdateHandler.go
@@ -2,13 +2,19 @@ package handlers
 
 import (
 	"UPDATE_WH/service"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UpdateHandler(server *gin.Engine, service service.IUpdateService) {
 	server.POST("/update", func(c *gin.Context) {
+		process, err := strconv.ParseBool(c.GetHeader("process"))
+		if err != nil {
+			process = false
+		}
+
 		ctx := c.Request.Context()
-		c.JSON(200, service.UpdateWh(ctx))
+		c.JSON(200, service.UpdateWh(ctx, process))
 	})
 }
--- service/service.go
+++ service/service.go
@@ -13,14 +13,14 @@ import (
 )
 
 var (
-	BaseUrl       = "https://internal-api.mercadolibre.com/shipping/selfservice/adoption/search"
+	BaseUrlFlex   = "https://internal-api.mercadolibre.com/shipping/selfservice/adoption/search"
 	BaseUrlWh     = "https://internal-api.mercadolibre.com/shipping/working-hours"
 	BaseUrlUpdate = "https://internal-api.mercadolibre.com/test/shipping/working-hours/services/"
-	SITES         = []string{"MLB", "MLU"}
+	SITES         = []string{"MLA", "MLB", "MLC", "MLU", "MLM", "MCO"}
 )
 
 type IUpdateService interface {
-	UpdateWh(ctx context.Context) string
+	UpdateWh(ctx context.Context, process bool) string
 }
 
 type RustyClient interface {
@@ -37,138 +37,16 @@ func NewPS(client RustyClient) IUpdateService {
 	}
 }
 
-func (u *UpdateService) UpdateWh(ctx context.Context) string {
+func (u *UpdateService) UpdateWh(ctx context.Context, process bool) string {
 	fmt.Println("--------------START------------------")
-	workingHours := serviceWhHttp(u.Client, ctx)
-	serviceBySite := fecthMapServices(u.Client, ctx)
-
-	var idsToProcess []int
-	var idsNotProcess []int
-	for _, id := range workingHours {
-		for _, site := range serviceBySite {
-			if !contains(site, id) {
-				idsToProcess = append(idsToProcess, id)
-			} else {
-				idsNotProcess = append(idsNotProcess, id)
-			}
-		}
-	}
-
-	fmt.Println("A procesar:", len(idsToProcess))
-	fmt.Println("No procesar:", idsNotProcess)
-
-	var idsWithErrors []int
-	/*for _, id := range idsToProcess {
-		if err := processIds(id, u.Client, ctx); err != nil {
-			idsWithErrors = append(idsWithErrors, id)
-		}
-	}*/
-
-	err := processIds(157861, u.Client, ctx)
-	fmt.Println("Error:", err.Error())
-	if len(idsWithErrors) > 0 {
-		return "Proceso terminado con errores"
-	}
-	return "Proceso terminado exitosamente!!"
+	workingHours := fectServiceWH(u.Client, ctx)
+	serviceBySite := fecthServiceFlex(u.Client, ctx)
+	idsToProcess := sortDataProcessed(workingHours, serviceBySite)
+	return processData(u.Client, ctx, process, idsToProcess)
 }
 
-func processIds(id int, Client RustyClient, ctx context.Context) error {
-	day := entity.Day{
-		Ranges: []string{"0000-2359"},
-	}
-	object := entity.RequestWh{
-		Id:        strconv.Itoa(id),
-		Monday:    day,
-		Tuesday:   day,
-		Wednesday: day,
-		Thursday:  day,
-		Friday:    day,
-		Saturday:  day,
-		Sunday:    day,
-	}
-
-	return serviceUpdateWhHttp(Client, object, ctx)
-}
-
-func contains(id []int, searchterm int) bool {
-	i := sort.SearchInts(id, searchterm)
-	return i < len(id) && id[i] == searchterm
-}
-
-func fecthMapServices(Client RustyClient, ctx context.Context) map[string][]int {
-	fmt.Println("Trallendo servicios externos")
-	m := make(map[string][]int)
-	for _, site := range SITES {
-		object := entity.Request{
-			Type: "scroll",
-			Query: entity.Query{
-				AnyEquals: []entity.AnyEquals{
-					{
-						Path:   "services.status.id",
-						Values: []string{"in", "pending"},
-					},
-				},
-				Equals: []entity.Equals{
-					{
-						Path:  "is_test",
-						Value: false,
-					},
-					{
-						Path:  "site_id",
-						Value: site,
-					},
-				},
-			},
-			Size:      100,
-			ContextId: "",
-		}
-		ids := fetchIdsBySite(site, Client, object, ctx)
-		fmt.Printf("site:%v  rows: %d\n", site, len(ids))
-		m[site] = ids
-	}
-	return m
-}
-
-func fetchIdsBySite(site string, Client RustyClient, object entity.Request, ctx context.Context) []int {
-	query := []entity.Response{}
-	query1 := serviceHttp(Client, object, ctx)
-	tope := query1.Total / 100
-	query = append(query, query1)
-
-	for i := 0; i < tope; i++ {
-		object.ContextId = query1.ContextId
-		query2 := serviceHttp(Client, object, ctx)
-		query = append(query, query2)
-	}
-	var ids []int
-	for _, q := range query {
-		for _, d := range q.Documents {
-			for _, s := range d.Services {
-				ids = append(ids, s.Id)
-			}
-		}
-	}
-	return ids
-}
-
-func serviceHttp(Client RustyClient, object interface{}, ctx context.Context) entity.Response {
-	cl, err := rusty.NewEndpoint(Client, BaseUrl, rusty.WithHeader("Content-Type", "application/json"))
-	if err != nil {
-		fmt.Println("Error:", err)
-		return entity.Response{}
-	}
-	res, err := cl.Post(ctx, rusty.WithBody(object))
-	if err != nil {
-		fmt.Println("Error:", err)
-		return entity.Response{}
-	}
-	response := entity.Response{}
-	json.Unmarshal(res.Body, &response)
-	return response
-}
-
-func serviceWhHttp(Client RustyClient, ctx context.Context) []int {
-	fmt.Println("Trallendo WH")
+func fectServiceWH(Client RustyClient, ctx context.Context) []int {
+	fmt.Println("Trallendo servicios WH")
 	cl, err := rusty.NewEndpoint(Client, BaseUrlWh, rusty.WithHeader("Content-Type", "application/json"))
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -194,8 +72,138 @@ func serviceWhHttp(Client RustyClient, ctx context.Context) []int {
 	return ids
 }
 
-func serviceUpdateWhHttp(Client RustyClient, object entity.RequestWh, ctx context.Context) error {
-	fmt.Println("Editando Ids")
+func fecthServiceFlex(Client RustyClient, ctx context.Context) map[string][]int {
+	fmt.Println("Trallendo servicios flex")
+	m := make(map[string][]int)
+	for _, site := range SITES {
+		object := entity.Request{
+			Type: "scroll",
+			Query: entity.Query{
+				AnyEquals: []entity.AnyEquals{
+					{
+						Path:   "services.status.id",
+						Values: []string{"in", "pending"},
+					},
+				},
+				Equals: []entity.Equals{
+					{
+						Path:  "is_test",
+						Value: false,
+					},
+					{
+						Path:  "site_id",
+						Value: site,
+					},
+				},
+			},
+			Size:      100,
+			ContextId: "",
+		}
+		ids := fetchIdsBySite(Client, ctx, site, object)
+		fmt.Printf("site:%v  rows: %d\n", site, len(ids))
+		m[site] = ids
+	}
+	return m
+}
+
+func fetchIdsBySite(Client RustyClient, ctx context.Context, site string, object entity.Request) []int {
+	query := []entity.Response{}
+	query1 := serviceFlexHttp(Client, ctx, object)
+	tope := query1.Total / 100
+	query = append(query, query1)
+
+	for i := 0; i < tope; i++ {
+		object.ContextId = query1.ContextId
+		query2 := serviceFlexHttp(Client, ctx, object)
+		query = append(query, query2)
+	}
+	var ids []int
+	for _, q := range query {
+		for _, d := range q.Documents {
+			for _, s := range d.Services {
+				ids = append(ids, s.Id)
+			}
+		}
+	}
+	return ids
+}
+
+func serviceFlexHttp(Client RustyClient, ctx context.Context, object interface{}) entity.Response {
+	cl, err := rusty.NewEndpoint(Client, BaseUrlFlex, rusty.WithHeader("Content-Type", "application/json"))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return entity.Response{}
+	}
+	res, err := cl.Post(ctx, rusty.WithBody(object))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return entity.Response{}
+	}
+	response := entity.Response{}
+	json.Unmarshal(res.Body, &response)
+	return response
+}
+
+func sortDataProcessed(workingHours []int, serviceBySite map[string][]int) []int {
+	fmt.Println("Buscando coincidencias..")
+	var idsToProcess []int
+	var idsNotProcess []int
+	for _, id := range workingHours {
+		for _, site := range serviceBySite {
+			if !contains(site, id) {
+				idsToProcess = append(idsToProcess, id)
+			} else {
+				idsNotProcess = append(idsNotProcess, id)
+			}
+		}
+	}
+	fmt.Println("A procesar:", len(idsToProcess))
+	fmt.Println("No procesar:", len(idsNotProcess))
+	return idsToProcess
+}
+
+func contains(id []int, searchterm int) bool {
+	i := sort.SearchInts(id, searchterm)
+	return i < len(id) && id[i] == searchterm
+}
+
+func processData(Client RustyClient, ctx context.Context, process bool, idsToProcess []int) string {
+	fmt.Println("Procesando datos..", process)
+	var idsWithErrors []int
+	if process {
+		for _, id := range idsToProcess {
+			if err := processIds(Client, ctx, id); err != nil {
+				idsWithErrors = append(idsWithErrors, id)
+			}
+		}
+	}
+	fmt.Println("Total de id con errores:", len(idsWithErrors))
+	fmt.Println("---------------END-------------------")
+	if process && len(idsWithErrors) > 0 {
+		return "Proceso terminado con errores"
+	}
+	return "Proceso terminado exitosamente!!"
+}
+
+func processIds(Client RustyClient, ctx context.Context, id int) error {
+	day := entity.Day{
+		Ranges: []string{"0000-2359"},
+	}
+	object := entity.RequestWh{
+		Id:        strconv.Itoa(id),
+		Monday:    day,
+		Tuesday:   day,
+		Wednesday: day,
+		Thursday:  day,
+		Friday:    day,
+		Saturday:  day,
+		Sunday:    day,
+	}
+
+	return updateServiceWh(Client, ctx, object)
+}
+
+func updateServiceWh(Client RustyClient, ctx context.Context, object entity.RequestWh) error {
 	cl, err := rusty.NewEndpoint(Client, BaseUrlUpdate+"/"+object.Id,
 		rusty.WithHeader("Content-Type", "application/json"),
 		rusty.WithHeader("X-Caller-Scopes", "admin"))
